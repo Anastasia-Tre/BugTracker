c15adee35c8164712c63b2e3e0bd6f9a33796c42
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-08-03 15:52:47 +0300

    add DependenciesEFData

---
+++ BugTracker.DataAccessLayer/DependenciesEFData.cs
@@ -0,0 +1,21 @@
+﻿using BugTracker.DataAccessLayer.Repositories.Abstraction;
+using BugTracker.DataAccessLayer.Repositories.Implementation.EFImplementation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BugTracker.DataAccessLayer
+{
+    public static class DependenciesEFData
+    {
+        public static IServiceCollection SetEFDataDependencies<TKey>(
+            this IServiceCollection services)
+        {
+            services.AddScoped<BugTrackerDbContext<TKey>>();
+            services.AddScoped<IBugRepository<TKey>, EFBugRepository<TKey>>();
+            services.AddScoped<IUserRepository<TKey>, EFUserRepository<TKey>>();
+            services.AddScoped<IProjectRepository<TKey>, EFProjectRepository<TKey>>();
+            services.AddScoped<IUserProjectRepository<TKey>, EFUserProjectRepository<TKey>>();
+            
+            return services;
+        }
+    }
+}
