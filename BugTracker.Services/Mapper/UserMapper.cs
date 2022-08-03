f7157e17aaef459cc04faeb053bf2195d7aa5fd6
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-08-03 19:56:49 +0300

    add UserMapper

---
+++ BugTracker.Services/Mapper/UserMapper.cs
@@ -0,0 +1,30 @@
+﻿using BugTracker.DataAccessLayer.Entities;
+using BugTracker.DataModel;
+
+namespace BugTracker.Services.Mapper
+{
+    public static class UserMapper
+    {
+        public static User<TKey> ToModelEntity<TKey>(this UserEntity<TKey> user)
+        {
+            return new User<TKey>()
+            {
+                Id = user.Id,
+                Name = user.Name,
+                Email = user.Email,
+                Password = user.Password,
+            };
+        }
+
+        public static UserEntity<TKey> ToDbEntity<TKey>(this User<TKey> user)
+        {
+            return new UserEntity<TKey>()
+            {
+                Id = user.Id,
+                Name = user.Name,
+                Email = user.Email,
+                Password = user.Password,
+            };
+        }
+    }
+}
