2dabc4e39e7f6e6cdf33fb73cc758777b3956d44
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-08-03 14:38:23 +0300

    add class EFUserRepository

---
+++ BugTracker.DataAccessLayer/Repositories/Implementation/EFImplementation/EFUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using BugTracker.DataAccessLayer.Entities;
+using BugTracker.DataAccessLayer.Repositories.Abstraction;
+
+namespace BugTracker.DataAccessLayer.Repositories.Implementation.EFImplementation
+{
+    public class EFUserRepository<TKey> : EFGenericRepository<UserEntity<TKey>, TKey>, IUserRepository<TKey>
+    {
+        public EFUserRepository(BugTrackerDbContext<TKey> dbContext) : base(dbContext, dbContext.Users)
+        {
+        }
+    }
+}
