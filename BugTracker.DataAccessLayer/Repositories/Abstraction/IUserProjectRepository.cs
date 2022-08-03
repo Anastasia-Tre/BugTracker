11906a0a9330230b847a868859c9ffb8c95fa241
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-08-03 14:53:26 +0300

    add IUserProjectRepository

---
+++ BugTracker.DataAccessLayer/Repositories/Abstraction/IUserProjectRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using BugTracker.DataAccessLayer.Entities;
+
+namespace BugTracker.DataAccessLayer.Repositories.Abstraction
+{
+    public interface IUserProjectRepository<TKey> : IRepository<UserProjectEntity<TKey>, TKey>
+    {
+        public IEnumerable<UserEntity<TKey>> GetUsersForProject(TKey projectId);
+        public IEnumerable<ProjectEntity<TKey>> GetProjectsForUser(TKey userId);
+    }
+}
