d92640d22a764e24b1f2d55ed4ce334440cb087a
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-08-03 14:06:19 +0300

    add methods to EFGenericRepository

--- BugTracker.DataAccessLayer/Repositories/Implementation/EFImplementation/EFGenericRepository.cs
+++ BugTracker.DataAccessLayer/Repositories/Implementation/EFImplementation/EFGenericRepository.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using BugTracker.DataAccessLayer.Entities;
+using BugTracker.DataAccessLayer.Repositories.Abstraction;
 
 namespace BugTracker.DataAccessLayer.Repositories.Implementation.EFImplementation
 {
-    public class EFGenericRepository
+    public class EFGenericRepository<T, TKey> : IRepository<T, TKey> where T : BaseEntity<TKey>
     {
+        public void Create(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(TKey id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T item)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
