5a105eb611209419e6221f81101be2fdab01195e
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-08-03 12:47:49 +0300

    add BugEntity

---
+++ BugTracker.DataAccessLayer/Entities/BugEntity.cs
@@ -0,0 +1,26 @@
+﻿namespace BugTracker.DataAccessLayer.Entities
+{
+    public class BugEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public BugStatus Status { get; set; }
+        public BugType Type { get; set; }
+        public BugPriority Priority { get; set; }
+
+        public int AuthorId { get; set; }
+
+        //public List<User> AssignToIds { get; set; }
+        public int ProjectId { get; set; }
+
+        public string Date { get; set; }
+        public float Estimate { get; set; }
+    }
+
+    public class BugUser
+    {
+    }
+}
