1384970b0867165b5ade602f09df6d41dfb49159
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-08-03 12:26:38 +0300

    update User class

--- BugTracker.DataModel/User.cs
+++ BugTracker.DataModel/User.cs
@@ -1,16 +1,11 @@
 ﻿namespace BugTracker.DataModel
 {
-    public class User
+    public class User<TKey>
     {
-        public int Id { get; set; }
+        public TKey Id { get; set; }
 
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-
-        //public List<Project> Projects { get; set; }
-
-        //public List<int> BugIds { get; set; }
-        //public List<Bug> Bugs { get; set; }
     }
 }
