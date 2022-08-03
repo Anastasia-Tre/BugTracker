0866674c3e0dfe7e601922dcb8bab0341949a95e
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-08-03 14:41:50 +0300

    add IProjectRepository

---
+++ BugTracker.DataAccessLayer/Repositories/Abstraction/IProjectRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using BugTracker.DataAccessLayer.Entities;
+
+namespace BugTracker.DataAccessLayer.Repositories.Abstraction
+{
+    public interface IProjectRepository<TKey> : IRepository<ProjectEntity<TKey>, TKey>
+    {
+        public IEnumerable<ProjectEntity<TKey>> Search(string searchString = "");
+        public IEnumerable<ProjectEntity<TKey>> GetProjectsForUser(TKey userId);
+    }
+}
