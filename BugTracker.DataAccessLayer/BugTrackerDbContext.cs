24f748e0b26287ca7d05aa1b6ce318438a733ce1
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-08-03 14:14:23 +0300

    fill class BugTrackerDbContext

--- BugTracker.DataAccessLayer/BugTrackerDbContext.cs
+++ BugTracker.DataAccessLayer/BugTrackerDbContext.cs
@@ -1,8 +1,19 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BugTracker.DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace BugTracker.DataAccessLayer
 {
-    public class BugTrackerDbContext : DbContext
+    public sealed class BugTrackerDbContext : DbContext
     {
+        public BugTrackerDbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        // ????? TKey ??????
+        public DbSet<BugEntity<int>> Bugs { get; set; }
+        public DbSet<UserEntity<int>> Users { get; set; }
+        public DbSet<ProjectEntity<int>> Projects { get; set; }
+        public DbSet<UserProjectEntity<int>> UserProjects { get; set; }
     }
 }
