5a0a22c782dc5a2b94f1414cc056188cc35f40c8
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-08-03 19:58:13 +0300

    add BugService

---
+++ BugTracker.Services/Implementation/BugService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BugTracker.DataModel;
+using BugTracker.Services.Abstraction;
+
+namespace BugTracker.Services.Implementation
+{
+    public class BugService<TKey> : IBugService<TKey>
+    {
+        public Bug<TKey> GetBugById(TKey id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Bug<TKey>> SearchBugs(string searchString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Bug<TKey>> GetBugsForProject(TKey projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Bug<TKey>> GetBugsForUser(TKey userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AssignBugToUser(Bug<TKey> bug, TKey userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
