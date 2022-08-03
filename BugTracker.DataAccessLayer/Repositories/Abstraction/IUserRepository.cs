277e15d297a82edafb72bf68ba9bc2c243c830c4
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-08-03 14:35:17 +0300

    add IUserRepository

---
+++ BugTracker.DataAccessLayer/Repositories/Abstraction/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using BugTracker.DataAccessLayer.Entities;
+
+namespace BugTracker.DataAccessLayer.Repositories.Abstraction
+{
+    public interface IUserRepository<TKey> : IRepository<UserEntity<TKey>, TKey>
+    {
+
+    }
+}
