dd93e2d270d23458bfd4d03cf49b42f51a9fe2fd
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-08-03 15:30:25 +0300

    add EFUserProjectRepository

---
+++ BugTracker.DataAccessLayer/Repositories/Implementation/EFImplementation/EFUserProjectRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using BugTracker.DataAccessLayer.Entities;
+using BugTracker.DataAccessLayer.Repositories.Abstraction;
+
+namespace BugTracker.DataAccessLayer.Repositories.Implementation.EFImplementation
+{
+    public class EFUserProjectRepository<TKey> : EFRepository<UserProjectEntity<TKey>, TKey>, IUserProjectRepository<TKey>
+    {
+        public EFUserProjectRepository(BugTrackerDbContext<TKey> dbContext) : base(dbContext, dbContext.UserProjects)
+        {
+        }
+
+        public IEnumerable<UserEntity<TKey>> GetUsersForProject(TKey projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ProjectEntity<TKey>> GetProjectsForUser(TKey userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
