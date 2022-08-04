c27d1ae7234911bb24c995ce29833ccdee9284c0
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-08-04 17:06:24 +0300

    add response for bug

---
+++ BugTracker.WebAPI/Model/Response/Bug/BugsResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using BugTracker.DataModel;
+
+namespace BugTracker.WebAPI.Model.Response.Bug
+{
+    public class BugsResponse
+    {
+        public List<Bug<int>> Bugs { get; set; }
+    }
+}
