150befbc0fe745bd9c40740c7685d4fa956f951b
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-08-04 17:57:04 +0300

    update Startup class

--- BugTracker.WebAPI/Startup.cs
+++ BugTracker.WebAPI/Startup.cs
@@ -1,16 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using BugTracker.DataAccessLayer;
+using BugTracker.Services;
 
 namespace BugTracker.WebAPI
 {
@@ -26,6 +21,9 @@ namespace BugTracker.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //fix TKey
+            services.SetEFDataDependencies<int>();
+            services.SetServices<int>();
 
             services.AddControllers();
             services.AddSwaggerGen(c => {
