e82bcd4b8c386d5f9c7216d52afa4a38b2f37d9a
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-08-04 14:50:48 +0300

    implement ProjectService

---
+++ BugTracker.Services/Implementation/ProjectService.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using AutoMapper;
+using BugTracker.DataAccessLayer.UnitOfWork.Abstraction;
+using BugTracker.DataModel;
+using BugTracker.Services.Abstraction;
+
+namespace BugTracker.Services.Implementation
+{
+    internal class ProjectService<TKey> : IProjectService<TKey>
+    {
+        private readonly IUnitOfWork<TKey> _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public ProjectService(IUnitOfWork<TKey> unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public Project<TKey> GetBugById(TKey id)
+        {
+            return _mapper.Map<Project<TKey>>(
+                _unitOfWork.BugRepository.GetById(id));
+        }
+
+        public IEnumerable<Project<TKey>> SearchBugs(string searchString)
+        {
+            return _mapper.Map<IEnumerable<Project<TKey>>>(
+                _unitOfWork.BugRepository.Search(searchString));
+        }
+    }
+}
