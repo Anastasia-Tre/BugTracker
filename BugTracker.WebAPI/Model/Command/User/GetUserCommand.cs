a1fbe87ea9064da51e98e23187be4a28a022ace3
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-08-04 17:23:18 +0300

    add commands for user

---
+++ BugTracker.WebAPI/Model/Command/User/GetUserCommand.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BugTracker.WebAPI.Model.Command.User
+{
+    public class GetUserCommand
+    {
+        [Required]
+        public int UserId { get; set; }
+    }
+}
