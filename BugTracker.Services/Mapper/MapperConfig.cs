4509a1a41e7d3d0ded5c66a2706044f2e2e4b631
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-08-04 14:31:11 +0300

    add MapperConfig

---
+++ BugTracker.Services/Mapper/MapperConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using BugTracker.DataAccessLayer.Entities;
+using BugTracker.DataModel;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BugTracker.Services.Mapper
+{
+    public static class MapperConfig
+    {
+        public static IServiceCollection SetMapperConfig<TKey>(this IServiceCollection services)
+        {
+            services.AddAutoMapper(cfg =>
+                cfg.CreateMap<Bug<TKey>, BugEntity<TKey>>().ReverseMap());
+            services.AddAutoMapper(cfg =>
+                cfg.CreateMap<Project<TKey>, ProjectEntity<TKey>>().ReverseMap());
+            services.AddAutoMapper(cfg =>
+                cfg.CreateMap<User<TKey>, UserEntity<TKey>>().ReverseMap());
+
+            return services;
+        }
+    }
+}
