bdfa75f638172039afd14ed8d52d22463ec16af6
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-08-04 17:35:54 +0300

    fix naming in ProjectService

--- BugTracker.Services/Abstraction/IProjectService.cs
+++ BugTracker.Services/Abstraction/IProjectService.cs
@@ -5,8 +5,8 @@ namespace BugTracker.Services.Abstraction
 {
     public interface IProjectService<TKey>
     {
-        public Project<TKey> GetBugById(TKey id);
-        public IEnumerable<Project<TKey>> SearchBugs(string searchString);
+        public Project<TKey> GetProjectById(TKey id);
+        public IEnumerable<Project<TKey>> SearchProjects(string searchString);
 
         // public Project<TKey> CreateProject();
         // public Project<TKey> DeleteProject(TKey id);
