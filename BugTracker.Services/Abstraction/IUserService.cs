eb060629b9b77fe2f22f5379335042792231c199
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-08-04 14:45:51 +0300

    implement UserService

--- BugTracker.Services/Abstraction/IUserService.cs
+++ BugTracker.Services/Abstraction/IUserService.cs
@@ -8,8 +8,8 @@ namespace BugTracker.Services.Abstraction
         public User<TKey> GetUserById(TKey id);
         public IEnumerable<User<TKey>> GetAllUsers();
 
-        public User<TKey> CreateUser();
-        public User<TKey> UpdateUser(TKey id, User<TKey> user);
-        public User<TKey> DeleteUser(TKey id);
+        // public User<TKey> CreateUser();
+        // public User<TKey> UpdateUser(TKey id, User<TKey> user);
+        // public User<TKey> DeleteUser(TKey id);
     }
 }
