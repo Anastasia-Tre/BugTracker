6b65e7bc2ad642a90efdf87aaab24a5dc13854f3
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-08-04 14:41:38 +0300

    add IUserService

---
+++ BugTracker.Services/Abstraction/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using BugTracker.DataModel;
+
+namespace BugTracker.Services.Abstraction
+{
+    internal interface IUserService<TKey>
+    {
+        public User<TKey> GetUserById(TKey id);
+        public IEnumerable<User<TKey>> GetAllUsers();
+
+        public User<TKey> CreateUser();
+        public User<TKey> UpdateUser(TKey id, User<TKey> user);
+        public User<TKey> DeleteUser(TKey id);
+    }
+}
