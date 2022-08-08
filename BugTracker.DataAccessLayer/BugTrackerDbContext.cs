cd462cbbcf2e84b6641f6467ba94d219f8020375
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-08-08 20:02:42 +0300

    fix connection to db

--- BugTracker.DataAccessLayer/BugTrackerDbContext.cs
+++ BugTracker.DataAccessLayer/BugTrackerDbContext.cs
@@ -1,20 +1,32 @@
-﻿using BugTracker.DataAccessLayer.Entities;
+﻿using BugTracker.DataAccessLayer.Configuration;
+using BugTracker.DataAccessLayer.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace BugTracker.DataAccessLayer
 {
     public sealed class BugTrackerDbContext : DbContext
     {
-        public BugTrackerDbContext(
-            DbContextOptions<BugTrackerDbContext> options) : base(options)
-        {
-            Database.EnsureDeleted();
-            Database.EnsureCreated();
-        }
+        private string _connectionString;
+        private static string DbDefaultConnectionString => new DatabaseConfiguration()
+            .GetDatabaseConnectionString();
 
         public DbSet<BugEntity<int>> Bugs { get; set; }
         public DbSet<UserEntity<int>> Users { get; set; }
         public DbSet<ProjectEntity<int>> Projects { get; set; }
         public DbSet<UserProjectEntity<int>> UserProjects { get; set; }
+
+        public BugTrackerDbContext(string connectionString = null) : base()
+        {
+            _connectionString = connectionString ?? DbDefaultConnectionString;
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(_connectionString);
+            //.UseExceptionProcessor();
+        }
     }
 }
--- BugTracker.DataAccessLayer/DependenciesEFData.cs
+++ BugTracker.DataAccessLayer/DependenciesEFData.cs
@@ -3,22 +3,16 @@ using BugTracker.DataAccessLayer.Repositories.Abstraction;
 using BugTracker.DataAccessLayer.Repositories.Implementation.EFImplementation;
 using BugTracker.DataAccessLayer.UnitOfWork.Abstraction;
 using BugTracker.DataAccessLayer.UnitOfWork.Implementation;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BugTracker.DataAccessLayer
 {
     public static class DependenciesEFData
     {
-        private static string DbConnectionString => new DatabaseConfiguration()
-            .GetDatabaseConnectionString();
-
         public static IServiceCollection SetEFDataDependencies(
-            this IServiceCollection services, string connectionString = null)
+            this IServiceCollection services)
         {
-            services.AddDbContext<BugTrackerDbContext>(options =>
-                options.UseSqlServer(connectionString ?? DbConnectionString));
-            //services.AddScoped<BugTrackerDbContext>();
+            services.AddDbContext<BugTrackerDbContext>();
             services.AddScoped<IBugRepository<int>, EFBugRepository>();
             services.AddScoped<IUserRepository<int>, EFUserRepository>();
             services.AddScoped<IProjectRepository<int>, EFProjectRepository>();
