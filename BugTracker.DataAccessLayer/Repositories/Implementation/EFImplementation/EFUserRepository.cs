1e950bf54f1cd81bf97cb45a1a08bb3595f8e4b6
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-08-13 13:27:53 +0300

    fix userRepository

--- BugTracker.DataAccessLayer/Repositories/Implementation/EFImplementation/EFUserRepository.cs
+++ BugTracker.DataAccessLayer/Repositories/Implementation/EFImplementation/EFUserRepository.cs
@@ -1,5 +1,8 @@
-﻿using BugTracker.DataAccessLayer.Entities;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BugTracker.DataAccessLayer.Entities;
 using BugTracker.DataAccessLayer.Repositories.Abstraction;
+using Microsoft.EntityFrameworkCore;
 
 namespace BugTracker.DataAccessLayer.Repositories.Implementation.
     EFImplementation
@@ -10,6 +13,12 @@ namespace BugTracker.DataAccessLayer.Repositories.Implementation.
         public EFUserRepository(BugTrackerDbContext dbContext) : base(dbContext,
             dbContext.Users)
         {
+            
+        }
+
+        public async Task<IEnumerable<UserEntity<int>>> GetAllUsers()
+        {
+            return await _entities.ToListAsync();
         }
     }
 }
