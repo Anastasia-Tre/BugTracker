c79f79faef85ce9eb064f45ab1efd1104076e69d
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-08-14 19:30:45 +0300

    add GetBugsByUserQuery

---
+++ BugTracker.WebAPI/Features/BugFeatures/Queries/GetBugsByUserQuery.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using BugTracker.DataModel;
+using BugTracker.Services.Abstraction;
+using MediatR;
+
+namespace BugTracker.WebAPI.Features.BugFeatures.Queries
+{
+    public class GetBugsByUserQuery : IRequest<IEnumerable<Bug<int>>>
+    {
+        public int UserId { get; set; }
+
+        public class GetBugsByUserQueryHandler : IRequestHandler<GetBugsByUserQuery, IEnumerable<Bug<int>>>
+        {
+            private readonly IBugService<int> _service;
+
+            public GetBugsByUserQueryHandler(IBugService<int> service)
+            {
+                _service = service;
+            }
+
+            public async Task<IEnumerable<Bug<int>>> Handle(GetBugsByUserQuery request, CancellationToken cancellationToken)
+            {
+                var result =
+                    await _service.GetBugsForUser(request.UserId);
+                return result;
+            }
+        }
+    }
+}
