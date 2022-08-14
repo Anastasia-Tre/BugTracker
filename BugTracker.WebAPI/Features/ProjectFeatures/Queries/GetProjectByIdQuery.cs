20c20b48d3a718cab9d9736e20e4dbe235467b71
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-08-14 14:52:18 +0300

    add GetProjectByIdQuery

---
+++ BugTracker.WebAPI/Features/ProjectFeatures/Queries/GetProjectByIdQuery.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using BugTracker.DataModel;
+using BugTracker.Services.Abstraction;
+using MediatR;
+
+namespace BugTracker.WebAPI.Features.ProjectFeatures.Queries
+{
+    public class GetProjectByIdQuery : IRequest<Project<int>>
+    {
+        public int ProjectId { get; set; }
+
+        public class GetProjectByIdQueryHandler : IRequestHandler<GetProjectByIdQuery, Project<int>>
+        {
+            private readonly IProjectService<int> _service;
+
+            public GetProjectByIdQueryHandler(IProjectService<int> service)
+            {
+                _service = service;
+            }
+            public async Task<Project<int>> Handle(GetProjectByIdQuery request, CancellationToken cancellationToken)
+            {
+                var result = await _service.GetProjectById(request.ProjectId);
+                return result;
+            }
+        }
+
+    }
+}
