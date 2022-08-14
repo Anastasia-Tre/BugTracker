68ce13247d6c9f9c068ad753798cfef88bbc7eb2
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-08-14 15:14:28 +0300

    add LoggingBehavior

---
+++ BugTracker.WebAPI/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace BugTracker.WebAPI.Behaviors
+{
+    public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger;
+
+        public LoggingBehavior(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            _logger.LogInformation($"Handling {typeof(TRequest).Name}");
+
+            var response = await next();
+
+            _logger.LogInformation($"Handled {typeof(TResponse).Name}");
+
+            return response;
+        }
+    }
+}
--- BugTracker.WebAPI/Startup.cs
+++ BugTracker.WebAPI/Startup.cs
@@ -2,6 +2,7 @@ using System.Reflection;
 using BugTracker.DataAccessLayer;
 using BugTracker.Services;
 using BugTracker.Services.Mapper;
+using BugTracker.WebAPI.Behaviors;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -31,6 +32,8 @@ namespace BugTracker.WebAPI
             services.SetServices();
 
             services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddSingleton(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
