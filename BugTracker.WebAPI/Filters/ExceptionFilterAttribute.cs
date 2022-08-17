1f03a21fbc440a5f65be3894746f032cdbebba8d
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-08-17 13:11:44 +0300

    add ExceptionFilterAttribute

---
+++ BugTracker.WebAPI/Filters/ExceptionFilterAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Hosting;
+
+namespace BugTracker.WebAPI.Filters
+{
+    public class ExceptionFilterAttribute: Attribute, IExceptionFilter
+    {
+        private readonly IHostEnvironment _hostEnvironment;
+
+        public ExceptionFilterAttribute(IHostEnvironment hostEnvironment) =>
+            _hostEnvironment = hostEnvironment;
+
+        public void OnException(ExceptionContext context)
+        {
+            if (!_hostEnvironment.IsDevelopment())
+            {
+                return;
+            }
+
+            context.Result = new ContentResult
+            {
+                Content = context.Exception.ToString()
+            };
+        }
+    }
+}
