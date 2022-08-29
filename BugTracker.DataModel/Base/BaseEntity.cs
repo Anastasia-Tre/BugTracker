efd534e9f16da7dc3f8b0105812a1d2c42d9e376
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-08-29 15:06:19 +0300

    add BaseEntity

---
+++ BugTracker.DataModel/Base/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BugTracker.DataModel.Base
+{
+    internal class BaseEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime ModifiedDate { get; private set; }
+
+        public BaseEntity()
+        {
+            ModifiedDate = DateTime.Now;
+        }
+    }
+}
