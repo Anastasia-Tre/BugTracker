1479fe678728112e04bec29612046afde962e361
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-09-02 17:47:46 +0300

    add AuthController

---
+++ BugTracker.WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using BugTracker.WebAPI.Features.AuthFeatures.Commands;
+using BugTracker.WebAPI.Filters;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BugTracker.WebAPI.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    [TypeFilter(typeof(ExceptionFilter))]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AuthController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> Login([FromBody] AuthCommand command)
+        {
+            return Ok(await _mediator.Send(command));
+        }
+    }
+}
---
+++ BugTracker.WebAPI/Features/AuthFeatures/Commands/AuthCommand.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using BugTracker.DataModel;
+using MediatR;
+
+namespace BugTracker.WebAPI.Features.AuthFeatures.Commands
+{
+    public class AuthCommand : IRequest<User<int>>
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public class
+            AuthCommandHandler : IRequestHandler<AuthCommand, User<int>>
+        {
+            public Task<User<int>> Handle(AuthCommand request, CancellationToken cancellationToken)
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+    }
+}
