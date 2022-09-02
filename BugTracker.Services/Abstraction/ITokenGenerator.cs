36b03ca09a5e4fc647cbf350ae439475c52b78ed
Anastasiya Trembach <nastia.trembach@gmail.com>
2022-09-02 18:10:43 +0300

    add TokenGenerator

---
+++ BugTracker.Services/Abstraction/ITokenGenerator.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace BugTracker.Services.Abstraction
+{
+    public interface ITokenGenerator
+    {
+        public string GenerateJWTToken(
+            (string userId, string username, IList<string> roles) userDetails);
+    }
+}
---
+++ BugTracker.Services/Implementation/TokenGenerator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using BugTracker.Services.Abstraction;
+
+namespace BugTracker.Services.Implementation
+{
+    public class TokenGenerator : ITokenGenerator
+    {
+        private readonly string _key;
+        private readonly string _issuer;
+        private readonly string _audience;
+        private readonly string _expiryMinutes;
+
+        public TokenGenerator(string key, string issuer, string audience, string expiryMinutes)
+        {
+            _key = key;
+            _issuer = issuer;
+            _audience = audience;
+            _expiryMinutes = expiryMinutes;
+        }
+
+        public string GenerateJWTToken((string userId, string username, IList<string> roles) userDetails)
+        {
+            var securityKey =
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_key));
+            var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>()
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userDetails.username),
+                new Claim(JwtRegisteredClaimNames.Jti, userDetails.userId),
+                new Claim(ClaimTypes.Name, userDetails.username),
+                new Claim("UserId", userDetails.userId)
+            };
+            claims.AddRange(userDetails.roles.Select(role => new Claim(ClaimTypes.Role, role)));
+
+            var token = new JwtSecurityToken(
+                issuer: _issuer,
+                audience: _audience,
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(
+                    Convert.ToDouble(_expiryMinutes)),
+                signingCredentials: signingCredentials
+            );
+
+            var encodedToken = new JwtSecurityTokenHandler().WriteToken(token);
+            return encodedToken;
+        }
+    }
+}
