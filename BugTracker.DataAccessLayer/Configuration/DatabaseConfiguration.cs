13d7324c19dbebdd114640214859241bf0665061
Anastasiya Trembach <nastia.trembach@gmail.com>
2023-05-21 13:02:31 +0300

    fix db connection

--- BugTracker.DataAccessLayer/Configuration/DatabaseConfiguration.cs
+++ BugTracker.DataAccessLayer/Configuration/DatabaseConfiguration.cs
@@ -1,4 +1,6 @@
-﻿namespace BugTracker.DataAccessLayer.Configuration;
+﻿using System;
+
+namespace BugTracker.DataAccessLayer.Configuration;
 
 internal class DatabaseConfiguration : ConfigurationBase
 {
@@ -7,13 +9,19 @@ internal class DatabaseConfiguration : ConfigurationBase
         var configuration =
             GetConfiguration().GetSection("DatabaseConnection");
         var server = configuration["DbServer"];
+        var port = configuration["DbPort"];
         var user = configuration["DbUser"];
         var password = configuration["DbPassword"];
         var database = configuration["DbName"];
 
+        Console.WriteLine($"Connection to db sever {server}");
+        //return
+        //    $"Server={server};Initial Catalog={database};User ID={user};Password={password};" +
+        //    "MultipleActiveResultSets=true;Connect Timeout=30;Encrypt=False;" +
+        //    "TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         return
-            $"Server={server};Initial Catalog={database};User ID={user};Password={password};" +
-            "MultipleActiveResultSets=true;Connect Timeout=30;Encrypt=False;" +
-            "TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            $"Server=tcp:{server},{port};Initial Catalog={database};Persist Security Info=False;" + 
+            $"User ID={user};Password={password};MultipleActiveResultSets=False;" + 
+            "Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
     }
 }
